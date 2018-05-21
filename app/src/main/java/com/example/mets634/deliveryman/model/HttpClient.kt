e0b52ee0e4acc349c34c730815fc791434e1a7a3
mets634 <mets634@gmail.com>
2018-05-21 18:06:00 +0300

    Some functionality created

---
+++ app/src/main/java/com/example/mets634/deliveryman/model/HttpClient.kt
@@ -0,0 +1,22 @@
+package com.example.mets634.deliveryman.model
+
+import timber.log.Timber
+import java.io.IOException
+import java.net.HttpURLConnection
+import java.net.HttpURLConnection.HTTP_OK
+import java.net.URL
+
+class HttpClient {
+    fun sendGet(url : String) : String {
+        Timber.d("Sending GET request to URL: $url")
+        val obj = URL(url)
+        val con = obj.openConnection() as HttpURLConnection
+
+        val responseCode = con.responseCode
+        if (responseCode != HTTP_OK)
+            throw IOException("GET request to URL: $url failed with response code: $responseCode")
+
+        Timber.d("Received response code: $responseCode from GET request to URL: $url")
+        return con.inputStream.reader().use { it.readText() }
+    }
+}
